d7de322df3180952afa3f4f4e12355df1cb393ac
V A Magendran <95860580+Magendran001@users.noreply.github.com>
2022-03-04 19:10:21 +0530

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("yes");
+const express = require("express");
+const res = require("express/lib/response");
+
+const app = express();
+
+app.get("/books", allBooks, (req, res) => {
+  try {
+    return res.send("All books");
+  } catch (e) {
+    console.log(e.message);
+  }
+});
+
+app.get("/books/:name", singleBook, (req, res) => {
+  try {
+    return res.send({ bookName: req.name });
+  } catch (e) {
+    console.log(e.message);
+  }
+});
+function singleBook(req, res, next) {
+  req.name = req.params.name;
+  next();
+}
+
+function allBooks(req, res, next) {
+  console.log("Fetching all books");
+  next();
+}
+
+app.listen(3452, () => {
+  console.log("3452 port");
+});
